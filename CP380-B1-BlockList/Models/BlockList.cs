f3f04729f65d842f1c9cfe3f865870879601453b
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-08-03 16:38:35 -0400

    Completed assignment 2A with output SS

--- CP380-B1-BlockList/Models/BlockList.cs
+++ CP380-B1-BlockList/Models/BlockList.cs
@@ -25,13 +25,22 @@ namespace CP380_B1_BlockList.Models
         public void AddBlock(Block block)
         {
             // TODO
+            block.PreviousHash = Chain[Chain.Count - 1].Hash;
+            block.Mine(Difficulty);
+            Chain.Add(block);
         }
 
         public bool IsValid()
         {
-            // TODO
-
-            return false;
+            for (var i = 1; i < Chain.Count; i++)
+            {
+                if (Chain[i].Hash.StartsWith("CC"))
+                {
+                    return false;
+                    
+                }
+            }
+            return true;
         }
     }
 }
